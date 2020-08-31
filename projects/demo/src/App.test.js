1fc64c99a1bf1c77f801182ce8a27dbaf0ddcc68
Michael Walter <michael@MacBook-Pro.local>
2020-08-30 23:35:05 -0700

    Working on 9.

--- projects/demo/src/App.test.js
+++ projects/demo/src/App.test.js
@@ -2,8 +2,11 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
+// This is the name of the test that
+// shows if it fails
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+  //throw new Error;
 });
