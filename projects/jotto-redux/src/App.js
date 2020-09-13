8abdc66aee127442443ca04af1c2f74b8848fa7b
Michael Walter <michael@MacBook-Pro.local>
2020-09-13 14:09:18 -0700

    Completed to 48. Implemented bootstrap styling in App.js and Congrats.js

--- projects/jotto-redux/src/App.js
+++ projects/jotto-redux/src/App.js
@@ -7,7 +7,7 @@ import Congrats from './Congrats';
 class App extends Component {
   render() {
     return (
-      <div data-test="component-app" className="App">
+      <div data-test="component-app" className="container">
         <h1>Jotto</h1>
         <Congrats success={true} />
         <GuessedWords guessedWords={[{guessedWord: 'train', letterMatchCount: 3}]} />
