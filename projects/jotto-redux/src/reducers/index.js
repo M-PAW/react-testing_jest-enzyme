39d69766a095da0276f6ace256fcbe1aa028b8c3
Michael Walter <michael_patrick.walter@outlook.com>
2020-09-17 17:38:02 -0700

    Built out the guessedWords reducer and added it to the combinedReducers function. Removed arbitrary initial test file /actions/index.test.js

--- projects/jotto-redux/src/reducers/index.js
+++ projects/jotto-redux/src/reducers/index.js
@@ -1,7 +1,9 @@
 
 import { combineReducers } from 'redux';
 import success from './successReducer';
+import guessedWords from './guessWordsReducer';
 
 export default combineReducers({
     success,
+    guessedWords,
 });
