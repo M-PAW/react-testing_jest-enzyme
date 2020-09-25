6dfd65f5ccfbdfaa96860d0d0a46d8f4e94c60bf
Michael Walter <michael_patrick.walter@outlook.com>
2020-09-24 22:16:01 -0700

    react-context - Built out the reducer via useReducer(), and implemented it using useEffect(). Built out reducer tests as red, built the reducer to make them green.

--- projects/jotto-context/src/App.js
+++ projects/jotto-context/src/App.js
@@ -4,14 +4,43 @@ import './App.css';
 import GuessedWords from './GuessedWords';
 import Congrats from './Congrats';
 import Input from './input';
+import hookActions from './actions/hookActions';
+
+/**
+ * reducer to update state, called automatically by dispatch
+ * @param {object} - existing state
+ * @param {object} - contains 'type' and 'payload' properties 
+ *                   for the state update
+ *    for example: { type: "setSecretWord", payload: "party" }
+ * @returns {object} - new state
+ */
+function reducer(state, action) {
+  switch(action.type) {
+    case "setSecretWord":
+      return { ...action, secretWord: action.payload }
+    default:
+      throw new Error(`Invalid action type: ${action.type}`)
+  }
+}
 
 function App() {
+
+  const [ state, dispatch ] = React.useReducer(
+    reducer,
+    { secretWord: null }
+  );
+
+  const setSecretWord = (secretWord) => 
+  dispatch({ type: "setSecretWord", payload: secretWord });
+
+  React.useEffect(
+    () => { hookActions.getSecretWord(setSecretWord) },
+    []
+  )
+
   return (
     <div data-test="component-app" className="container">
-      <h1>Jotto</h1>
-      <Congrats success={true} />
-      <Input secretWord="party" />
-      <GuessedWords guessedWords={[{guessedWord: 'train', letterMatchCount: 3}]} />
+      
     </div>
   );
 }
