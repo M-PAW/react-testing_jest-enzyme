a1cb39ae55068998382c6d74fdc6e26cdafc8c64
Michael Walter <michael_patrick.walter@outlook.com>
2020-09-25 20:11:50 -0700

    Built out initial red tests for languageContext, all were red. Built out Congrats, implemented languageContext and made them pass as green.

--- projects/jotto-context/src/App.js
+++ projects/jotto-context/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import languageContext from './contexts/languageContext';
 
+import languagePicker from './LanguagePicker';
 import GuessedWords from './GuessedWords';
 import Congrats from './Congrats';
 import Input from './input';
 import hookActions from './actions/hookActions';
+import LanguagePicker from './LanguagePicker';
 
 /**
  * reducer to update state, called automatically by dispatch
@@ -17,7 +20,9 @@ import hookActions from './actions/hookActions';
 function reducer(state, action) {
   switch(action.type) {
     case "setSecretWord":
-      return { ...action, secretWord: action.payload }
+      return { ...state, secretWord: action.payload }
+    case "setLanguage":
+      return { ...state, language: action.payload }
     default:
       throw new Error(`Invalid action type: ${action.type}`)
   }
@@ -27,11 +32,14 @@ function App() {
 
   const [ state, dispatch ] = React.useReducer(
     reducer,
-    { secretWord: null }
+    { secretWord: null, language: 'en' }
   );
 
   const setSecretWord = (secretWord) => 
-  dispatch({ type: "setSecretWord", payload: secretWord });
+    dispatch({ type: "setSecretWord", payload: secretWord });
+
+  const setLanguage = (language) =>
+    dispatch({ type: "setLanguage", payload: language });
 
   React.useEffect(
     () => { hookActions.getSecretWord(setSecretWord) },
@@ -51,7 +59,11 @@ function App() {
 
   return (
     <div className="container" data-test="component-app">
-      <Input secretWord={ state.secretWord } />
+      <h1>Jotto</h1>
+      <languageContext.Provider value={state.language}>
+        <LanguagePicker setLanguage={setLanguage} />
+        <Input secretWord={ state.secretWord } />
+      </languageContext.Provider>
     </div>
   );
 }
