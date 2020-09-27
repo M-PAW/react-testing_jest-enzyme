11cfcf3214d4a97b82625cea565df4ba55cb187c
Michael Walter <michael_patrick.walter@outlook.com>
2020-09-27 03:05:54 -0700

    Completed input component and linked the contexts, now guesses are registeres and number of matching letters are shown. Red tests for this were writen, and changed to green upon building out input.

--- projects/jotto-context/src/input.js
+++ projects/jotto-context/src/input.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import PropTypes, { string } from 'prop-types';
 
+import guessedWordsContext from './contexts/guessedWordsContext';
 import languageContext from './contexts/languageContext';
 import stringsModule from './helpers/strings';
 import successContext from './contexts/successContext';
+import { getLetterMatchCount } from './helpers';
 
 function Input ({ secretWord }) {
     const language = React.useContext(languageContext);
     const [ success, setSuccess ] = successContext.useSuccess();
+    const [ guessedWords, setGuessedWords ] = guessedWordsContext.useGuessedWords();
     const [ currentGuess, setCurrentGuess ] = React.useState("");
 
     if (success) { return null};
@@ -29,8 +32,17 @@ function Input ({ secretWord }) {
                     onClick={(evt) => {
                         evt.preventDefault();
                     
-                        // TODO: update guessedWords
-                        // TODO: check against secretWORD and update success if needed
+                        // update guessedWords
+                        const letterMatchCount = getLetterMatchCount(currentGuess, secretWord);
+                        const newGuessedWords = [ ...guessedWords, { guessedWord: currentGuess, letterMatchCount } ];
+                        setGuessedWords(newGuessedWords);
+
+                        // check against secretWORD and update success if needed
+                        if (currentGuess === secretWord) {
+                            setSuccess(true);
+                        }
+
+                        // Clearing the input box
                         setCurrentGuess("")}}
                     >
                     {stringsModule.getStringByLanguage(language, 'submit')}
